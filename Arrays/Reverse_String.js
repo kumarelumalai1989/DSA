c931ecec20928aed103c2cf41176d0143ec3f766
Kumar <35923820+kumarelumalai1989@users.noreply.github.com>
2025-10-12 22:22:09 +0530

    Update Reverse_String.js

--- Arrays/Reverse_String.js
+++ Arrays/Reverse_String.js
@@ -1,7 +1,6 @@
 // Reverse String : Write a function that reverses a string. The input string is given as an array of characters s
 // You must do this by modifying the input array in-place with O(1) extra memory
-
-function(s) {
+function Reverse_String(s) {
     let n = s.length;
     halflen = Math.floor(s.length/2);
     for(let i=0;i<halflen;i++)
